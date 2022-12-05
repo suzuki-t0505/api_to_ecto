bcee33ecd4381548132140479e4f0d933bfe81b8
suzuki-t0505 <takumi.s@thewaggle.co.jp>
2022-12-05 16:29:39 +0900

    修正

--- lib/api_to_ecto.ex
+++ lib/api_to_ecto.ex
@@ -14,8 +14,8 @@ defmodule ApiToEcto do
     %Place{
       name: mapdata["大字町丁目コード"],
       address: mapdata["都道府県名"] <> mapdata["市区町村名"] <> mapdata["大字町丁目名"],
-      lat: String.to_float(mapdata["経度"]),
-      lon: String.to_float(mapdata["緯度"])
+      lat: String.to_float(mapdata["緯度"]),
+      lon: String.to_float(mapdata["経度"])
     }
   end
 
